1763a05065ca1d20350d4a5c32a191de7bcdb389
Anas LARIBI <anas.laribi95@gmail.com>
2025-02-25 05:20:07 -0500

    Pages and Routing

--- Crypto Place/src/App.jsx
+++ Crypto Place/src/App.jsx
@@ -1,9 +1,16 @@
 import Navbar from "./Components/Navbar/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Coin from "./Pages/Coin/Coin";
 
 const App = () => {
   return (
     <div className="app">
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/coin/:coinId" element={<Coin />} />
+      </Routes>
     </div>
   );
 };
---
+++ Crypto Place/src/Pages/Home/Home.jsx
@@ -0,0 +1,21 @@
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="hero">
+        <h1>
+          Largest <br /> Crypto MarketPlace
+        </h1>
+        <p>
+          Welcome to the world&apos;s largest cryptocurrency MarketPlace. Sign
+          Up to explore more about cryptos
+        </p>
+        <input type="text" placeholder="Search Crypto" name="" id="" />
+        <button type="submit">Search</button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
